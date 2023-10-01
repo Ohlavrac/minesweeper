6cb8e6031c76b726c5a70c90eacb201ff198060e
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-01 10:20:23 -0300

    feat: add app texts

---
+++ lib/ui/screens/shared/app_texts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:minesweeper/ui/screens/shared/app_colors.dart';
+
+class AppText {
+  static final title = TextStyle(
+    fontSize: 24,
+    color: AppColors.textcolor,
+    fontWeight: FontWeight.bold
+  );
+}
