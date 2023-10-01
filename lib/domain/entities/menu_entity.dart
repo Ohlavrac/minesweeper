de5a3671a6aa62fbf43b631a7b00ad4da838c5d1
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-01 10:19:58 -0300

    feat: add createboard on menu entity

--- lib/domain/entities/menu_entity.dart
+++ lib/domain/entities/menu_entity.dart
@@ -1,4 +1,9 @@
+import 'package:minesweeper/domain/entities/board_entity.dart';
+
 class MenuEntity {
+  final int points = 0;
+  final int timer = 0;
+
   MenuEntity();
 
   String selectGameMode(int option) {
@@ -10,6 +15,14 @@ class MenuEntity {
       return "hard";
     } else {
       return "error";
+    }
+  }
+
+  BoardEntity createBoard(String gamemode) {
+    if (gamemode.toLowerCase() == "easy") {
+      return BoardEntity([], 8, 8);
+    } else {
+      return BoardEntity([], 24, 24);
     }
   }
 }
