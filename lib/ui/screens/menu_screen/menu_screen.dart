b087193cf3ba8ea87674b281aef9dc3a11c9b7ca
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-02 14:35:55 -0300

    feat: add menu screem

--- lib/ui/screens/menu_screen/menu_screen.dart
+++ lib/ui/screens/menu_screen/menu_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:minesweeper/ui/screens/shared/app_colors.dart';
-import 'package:minesweeper/ui/screens/shared/app_texts.dart';
+import 'package:minesweeper/ui/shared/app_colors.dart';
+import 'package:minesweeper/ui/shared/app_texts.dart';
+import 'package:minesweeper/ui/widgets/custom_menu_button_widget.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -9,18 +10,20 @@ class MenuScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundcolor,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("UltraMegaMineSweeper", style: AppText.title,),
-          ElevatedButton(
-            onPressed: () {
-
-            },
-            child: Text("FACIL"),
-          )
-        ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Text("UltraMegaMineSweeper", style: AppText.title,),
+            ),
+            CustomMenuButtonWidget(title: "FACIL", onPressed: () {}),
+            CustomMenuButtonWidget(title: "MEDIO", onPressed: () {}),
+            CustomMenuButtonWidget(title: "DIFICIL", onPressed: () {}),
+          ],
+        ),
       ),
     );
   }
