c140c8461df7e46955e610aecd5dc2b97df86856
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-02 15:18:09 -0300

    feat: add route named at menu buttons

--- lib/ui/screens/menu_screen/menu_screen.dart
+++ lib/ui/screens/menu_screen/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/domain/entities/menu_entity.dart';
 import 'package:minesweeper/ui/shared/app_colors.dart';
 import 'package:minesweeper/ui/shared/app_texts.dart';
 import 'package:minesweeper/ui/widgets/custom_menu_button_widget.dart';
@@ -8,6 +9,7 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    MenuEntity menu = MenuEntity();
     return Scaffold(
       backgroundColor: AppColors.backgroundcolor,
       body: Center(
@@ -19,9 +21,15 @@ class MenuScreen extends StatelessWidget {
               padding: const EdgeInsets.only(bottom: 50),
               child: Text("UltraMegaMineSweeper", style: AppText.title,),
             ),
-            CustomMenuButtonWidget(title: "FACIL", onPressed: () {}),
-            CustomMenuButtonWidget(title: "MEDIO", onPressed: () {}),
-            CustomMenuButtonWidget(title: "DIFICIL", onPressed: () {}),
+            CustomMenuButtonWidget(title: "FACIL", onPressed: () {
+              Navigator.pushNamed(context, "/game", arguments: menu.selectGameMode(1));
+            }),
+            CustomMenuButtonWidget(title: "MEDIO", onPressed: () {
+              Navigator.pushNamed(context, "/game", arguments: menu.selectGameMode(2));
+            }),
+            CustomMenuButtonWidget(title: "DIFICIL", onPressed: () {
+              Navigator.pushNamed(context, "/game", arguments: menu.selectGameMode(3));
+            }),
           ],
         ),
       ),
