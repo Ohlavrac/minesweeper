28a312f1d64b4cee659b0c5d6c58577c76037a63
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-02 14:35:31 -0300

    update

---
+++ lib/ui/shared/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final backgroundcolor = Color(0xff353130);
+  static final primary = Color(0xfff7345b);
+  static final textcolor = Colors.white;
+}
---
+++ lib/ui/shared/app_texts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:minesweeper/ui/shared/app_colors.dart';
+
+class AppText {
+  static final title = TextStyle(
+    fontSize: 24,
+    color: AppColors.textcolor,
+    fontWeight: FontWeight.bold
+  );
+
+  static final buttontitle = TextStyle(
+    fontSize: 18,
+    color: AppColors.textcolor,
+    fontWeight: FontWeight.w500,
+  );
+}
