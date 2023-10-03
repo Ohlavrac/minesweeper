06a5b414ab441991ba728ba3164fcaa444fb046f
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-03 16:46:04 -0300

    test: field mark and field has bomb

--- test/domain_tests/entities_tests/field_entity_test.dart
+++ test/domain_tests/entities_tests/field_entity_test.dart
@@ -1,7 +1,41 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:minesweeper/domain/entities/field_entity.dart';
 
 void main() {
-  test("Should return a field empty", () {
-    
+  test("Should return a field with a bomb", () {
+    FieldEntity field = FieldEntity(
+        hasBomb: true,
+        isChecked: false,
+        wasRevelated: false,
+        neighboringPumps: 0);
+    expect(field.hasBomb, true);
+  });
+
+  test("Should return a field without a bomb", () {
+    FieldEntity field = FieldEntity(
+        hasBomb: false,
+        isChecked: false,
+        wasRevelated: false,
+        neighboringPumps: 0);
+    expect(field.hasBomb, false);
+  });
+
+  test("Should return a field with a flag (is checked)", () {
+    FieldEntity field = FieldEntity(
+        hasBomb: true,
+        isChecked: true,
+        wasRevelated: false,
+        neighboringPumps: 0);
+    expect(field.isChecked, true);
+  });
+
+  test("Should remove the flag from the field", () {
+    FieldEntity field = FieldEntity(
+        hasBomb: true,
+        isChecked: true,
+        wasRevelated: false,
+        neighboringPumps: 0);
+
+    expect(field.markField(), false);
   });
 }
