d8bed2e4b338ba1cc0d7f92fda66f3cea048760c
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-03 22:30:12 -0300

    feat: add generate on createboard

--- lib/domain/entities/menu_entity.dart
+++ lib/domain/entities/menu_entity.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:minesweeper/domain/entities/board_entity.dart';
+import 'package:minesweeper/domain/entities/field_entity.dart';
 
 class MenuEntity {
   final int points = 0;
@@ -19,12 +22,28 @@ class MenuEntity {
   }
 
   BoardEntity createBoard(String gamemode) {
+    BoardEntity board;
+    FieldEntity field = FieldEntity(
+        hasBomb: false,
+        isChecked: false,
+        wasRevelated: false,
+        neighboringPumps: 0);
+        
     if (gamemode.toLowerCase() == "easy") {
-      return BoardEntity(fields: [],lines: 8,columns: 8, flags: 10, bombs: 10);
+      board =
+          BoardEntity(fields: [], lines: 8, columns: 8, flags: 10, bombs: 10);
+      field.generateFields(board);
+      return board;
     } else if (gamemode.toLowerCase() == "medium") {
-      return BoardEntity(fields: [],lines: 10,columns: 16, flags: 30, bombs: 30);
+      board =
+          BoardEntity(fields: [], lines: 10, columns: 16, flags: 30, bombs: 30);
+      field.generateFields(board);
+      return board;
     } else {
-      return BoardEntity(fields: [],lines: 24,columns: 24, flags: 100, bombs: 100);
+      board = BoardEntity(
+          fields: [], lines: 24, columns: 24, flags: 100, bombs: 100);
+      field.generateFields(board);
+      return board;
     }
   }
 }
