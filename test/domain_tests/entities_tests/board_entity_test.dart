d8c7bbbf6fc51b4597648e9994601b89c46ef1f4
ohlavrac <joaovicrcarvalho@gmail.com>
2023-10-06 15:44:05 -0300

    test: checked if the board has the correct number of fields for each game mode

--- test/domain_tests/entities_tests/board_entity_test.dart
+++ test/domain_tests/entities_tests/board_entity_test.dart
@@ -4,6 +4,11 @@ import 'package:minesweeper/domain/entities/field_entity.dart';
 import 'package:minesweeper/domain/errors/flags_error.dart';
 
 void main() {
+  FieldEntity fieldEntity = FieldEntity(
+      hasBomb: false,
+      isChecked: false,
+      wasRevelated: false,
+      neighboringPumps: 0);
   test("Should return a board line for EASY mode", () {
     BoardEntity board = BoardEntity(
         fields: [], lines: 8, columns: 8, flags: 10, bombs: 10, timer: 0);
@@ -93,16 +98,37 @@ void main() {
   test("Should return line and column of a select field", () {
     BoardEntity board = BoardEntity(
         fields: [], lines: 8, columns: 8, flags: 11, bombs: 10, timer: 0);
-    FieldEntity fieldEntity = FieldEntity(
-        hasBomb: false,
-        isChecked: false,
-        wasRevelated: false,
-        neighboringPumps: 0);
-
     fieldEntity.generateFields(board);
     int lineNumber = board.getLineNumber(board.columns, 5);
     int columnNumber = board.getColumnNumber(board.columns, 5);
 
     expect([lineNumber, columnNumber], [0, 5]);
+  });
+
+  test("Should return 64 fields from easy mode board", () {
+    BoardEntity board = BoardEntity(
+        fields: [], lines: 8, columns: 8, flags: 10, bombs: 10, timer: 0);
+
+    fieldEntity.generateFields(board);
+
+    expect(board.fields.length, 64);
+  });
+
+  test("Should return 160 fields from medium mode board", () {
+    BoardEntity board = BoardEntity(
+        fields: [], lines: 10, columns: 16, flags: 30, bombs: 30, timer: 0);
+
+    fieldEntity.generateFields(board);
+
+    expect(board.fields.length, 160);
+  });
+
+  test("Should return 576 fields from hard mode board", () {
+    BoardEntity board = BoardEntity(
+        fields: [], lines: 24, columns: 24, flags: 100, bombs: 100, timer: 0);
+
+    fieldEntity.generateFields(board);
+
+    expect(board.fields.length, 576);
   });
 }
